a7597168e1b59d4458d34f4cf9c125ddae96fea0
Audinovich <arieludinovich@gmail.com>
2024-05-30 21:41:26 +0200

    primer push

---
+++ src/p0/saludar.java
@@ -0,0 +1,13 @@
+package p0;
+
+
+public class saludar {
+	
+public static void main(String[] args) {
+	
+	System.out.println("HOLA MUNDO");
+	System.out.println("HOY ES "+ new java.util.Date());
+}
+	}
+
+
