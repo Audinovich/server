d33797bce5ab0d9b70df7b0affc0e98f162a1968
Audinovich <arieludinovich@gmail.com>
2024-05-31 11:53:37 +0200

    v 1.2

--- src/p0/saludar.java
+++ src/p0/saludar.java
@@ -5,7 +5,7 @@ public class saludar {
 	
 public static void main(String[] args) {
 	
-	System.out.println("HOLA MUNDO v1.0");
+	System.out.println("HOLA MUNDO v1.2 ");
 	System.out.println("HOY ES "+ new java.util.Date());
 }
 	}
