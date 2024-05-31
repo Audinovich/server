b097b7b3e52f357c1579957cf1c90cd6a088c43e
Audinovich <arieludinovich@gmail.com>
2024-05-31 12:44:15 +0200

    PRUEBA SUBIR INFOR EN RAMA

---
+++ src/p0/prueba_hub_rama.java
@@ -0,0 +1,12 @@
+package p0;
+
+public class prueba_hub_rama {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HOLA RAMA ");
+		System.out.println("HOY ES "+ new java.util.Date());
+	}
+		
+
+}
